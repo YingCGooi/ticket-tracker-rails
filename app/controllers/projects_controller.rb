d71afad2b8f62813e7d2d8fbe21e27630e5ddbae
YingCGooi <gooiyingchyi@gmail.com>
2018-08-06 19:22:51 -0400

    add CRUD functions to projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,4 @@
-class ProjectsController < ActionController::Base
+class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
@@ -6,5 +6,42 @@ class ProjectsController < ActionController::Base
   def show
     @project = Project.find(params[:id])
     @tickets = @project.tickets
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    # @project.creator = current_user
+
+    if @project.save
+      flash[:notice] = "Your project \"#{@project.name}\" was created."
+      redirect_to projects_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+
+    if @project.update(project_params)
+      flash[:notice] = "Your project was updated."
+      redirect_to project_path(@project)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description)
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,6 @@
 class Project < ActiveRecord::Base
   has_many :tickets
+
+  validates :name, presence: true, length: { minimum: 3 }, uniqueness: true
+  validates :description, presence: true
 end
