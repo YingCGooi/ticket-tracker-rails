e203052deb29901c35a15ab914ea94eafec552fb
YingCGooi <gooiyingchyi@gmail.com>
2018-08-09 20:33:42 -0400

    optimize ticket counts in project index

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,12 @@ class Project < ActiveRecord::Base
 
   validates :name, presence: true, length: { minimum: 3 }, uniqueness: true
   validates :description, presence: true
+
+  def self.open_tickets_count
+    self.joins(:tickets).where("tickets.status != 'fixed'").group(:id).count
+  end
+
+  def self.total_tickets_count
+    self.joins(:tickets).group(:id).count
+  end
 end
